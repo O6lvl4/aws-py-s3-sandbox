e6accdd91254b02ce3dd1de61e14345933bd1514
O6lvl4 <motodera@phismize.com>
2022-03-10 00:59:19 +0900

    Add type hinting for s3

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,11 +1,22 @@
 import json
 import boto3
+from boto3_type_annotations import s3
+
+
+BUCKET_NAME = "motodera.s3-python-sandbox.development.any"
+
+def upload_to_s3(bucket_name: str, upload_file_path: str, s3_key_name: str):
+    s3Resource: s3.ServiceResource = boto3.resource('s3')
+    bucket: s3.Bucket = s3Resource.Bucket(bucket_name)
+    bucket.upload_file(upload_file_path, s3_key_name)
+
+def download_at_s3(bucket_name: str, s3_key_name: str, download_file_path: str):
+    s3Resource: s3.ServiceResource = boto3.resource('s3')
+    bucket = s3Resource.Bucket(bucket_name)
+    bucket.download_file(s3_key_name, download_file_path)
 
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3')
-    bucket = s3.Bucket('motodera.s3-python-sandbox.development.any')
-    bucket.upload_file('fuga.txt', 'fuga.txt')
-    print(bucket)
+    upload_to_s3(BUCKET_NAME, 'fuga.txt', 'example/sample.txt')
     return {
         "statusCode": 200,
         "body": json.dumps({
@@ -13,3 +24,5 @@ def lambda_handler(event, context):
             # "location": ip.text.replace("\n", "")
         }),
     }
+
+download_at_s3(BUCKET_NAME, "example/sample.txt", "test.txt")
